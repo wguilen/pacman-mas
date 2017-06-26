88e4c0de5fdaf9de100b9272cef5c8c7deb18701
William Guilen <wguilen@outlook.com>
2017-06-26 16:55:04 -0300

    Added getNewPosition() validation about the size of the board

--- src/pacman/model/behaviour/GhostMovementBehaviour.java
+++ src/pacman/model/behaviour/GhostMovementBehaviour.java
@@ -38,12 +38,20 @@ public class GhostMovementBehaviour extends TickerBehaviour
     @Override
     public void onTick()
     {
+        // If the game is not running or the ghost still didn't 
+        //      left his house, doesn't move
         if (!(gameRunning && ((GhostAgent) myAgent).isHouseLeft()))
         {
             //System.out.println(myAgent.getLocalName() + " still didn't left his house...");
             return;
         }
-
+        
+        move();                 // Ghost makes a movement
+        checkGhostOnSamePath(); // Ghost checks if are there another ghosts on the same path
+    }
+    
+    private void move()
+    {
         boolean cellSelected = false;
         
         Coord2D myPosition = myCell.getPosition();
@@ -58,7 +66,7 @@ public class GhostMovementBehaviour extends TickerBehaviour
                 // Selects a direction to follow
                 for (Direction direction : Direction.values())
                 {
-                    myNewPosition = new Coord2D(myPosition.x + direction.xInc, myPosition.y + direction.yInc);
+                    myNewPosition = getNewPosition(myPosition, direction);
                     nearCell = board.getCell(myNewPosition);
 
                     // If it's a valid cell and the direction is not a reverse one, selects it
@@ -83,7 +91,7 @@ public class GhostMovementBehaviour extends TickerBehaviour
                         continue;
                     }
                     
-                    myNewPosition = new Coord2D(myPosition.x + direction.xInc, myPosition.y + direction.yInc);
+                    myNewPosition = getNewPosition(myPosition, direction);
                     nearCell = board.getCell(myNewPosition);
                     
                     // Found a valid bifurcation
@@ -108,7 +116,7 @@ public class GhostMovementBehaviour extends TickerBehaviour
                 // Else, tries to keep following the previous direction
                 if (!cellSelected)
                 {
-                    myNewPosition = new Coord2D(myPosition.x + currentDirection.xInc, myPosition.y + currentDirection.yInc);
+                    myNewPosition = getNewPosition(myPosition, currentDirection);
                     nearCell = board.getCell(myNewPosition);
 
                     // If the direction being followed is no longer valid, selects another
@@ -128,12 +136,44 @@ public class GhostMovementBehaviour extends TickerBehaviour
         board.moveCell(myCell, myNewPosition);
     }
     
+    private void checkGhostOnSamePath()
+    {
+        
+    }
+    
     private boolean isValidDestination(Cell cell)
     {
         return CellType.DOOR != cell.getType()             // Cannot run to a door
                && CellType.GHOST_HOUSE != cell.getType()   // Neither to a ghost house
                && CellType.GHOST != cell.getType()         // Neither to another ghost
                && CellType.WALL != cell.getType();         // Neither to a wall
+    }
+    
+    private Coord2D getNewPosition(Coord2D currentPosition, Direction destination)
+    {
+        Coord2D newPosition = new Coord2D(currentPosition.x + destination.xInc, currentPosition.y + destination.yInc);
+        
+        // Validates x position
+        if (newPosition.x < 0) 
+        {
+            newPosition = new Coord2D(board.countRows() - 1, newPosition.y);
+        }
+        else if (newPosition.x > board.countRows() - 1)
+        {
+            newPosition = new Coord2D(0, newPosition.y);
+        }
+        
+        // Validates y position        
+        if (newPosition.y < 0) 
+        {
+            newPosition = new Coord2D(newPosition.x, board.countColumns() - 1);
+        }
+        else if (newPosition.y > board.countColumns() - 1)
+        {
+            newPosition = new Coord2D(newPosition.x, 0);
+        }
+        
+        return newPosition;
     }
 
 }
